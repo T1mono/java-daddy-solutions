cea6e5147ff5863ea49d47e64d501d60502357c8
T1mono <167001770+T1mono@users.noreply.github.com>
2025-04-01 22:30:06 +0500

    Added VariableDemo

---
+++ src/main/java/ru/javadaddy/roadmap/step2_Base/topic2_Variables/task1_VariableDemo/VariableDemo.java
@@ -0,0 +1,12 @@
+package ru.javadaddy.roadmap.step2_Base.topic2_Variables.task1_VariableDemo;
+
+public class VariableDemo {
+    public static void main(String[] args) {
+        int first = 300_000;
+        double second = 234.23;
+        boolean isCheck = true;
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(isCheck);
+    }
+}
