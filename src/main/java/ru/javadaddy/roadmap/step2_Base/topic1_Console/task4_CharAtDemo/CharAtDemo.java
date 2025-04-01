6b0555184eab8b276b16a65caa1cd8dc01f27fa2
T1mono <167001770+T1mono@users.noreply.github.com>
2025-04-01 22:13:11 +0500

    Added CharAtDemo

---
+++ src/main/java/ru/javadaddy/roadmap/step2_Base/topic1_Console/task4_CharAtDemo/CharAtDemo.java
@@ -0,0 +1,9 @@
+package ru.javadaddy.roadmap.step2_Base.topic1_Console.task4_CharAtDemo;
+
+public class CharAtDemo {
+    public static void main(String[] args) {
+        String message = "Hello";
+        char ch = message.charAt(3);
+        System.out.println("Символ на позиции 3: " + ch);
+    }
+}
