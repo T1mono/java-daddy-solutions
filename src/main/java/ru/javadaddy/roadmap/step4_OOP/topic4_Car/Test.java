b1a3aa867c1a5e513b03280919c72860f42ee001
T1mon <godropopka@gmail.com>
2025-04-08 17:44:34 +0500

    Added package topic4_Car

---
+++ src/main/java/ru/javadaddy/roadmap/step4_OOP/topic4_Car/Test.java
@@ -0,0 +1,8 @@
+package ru.javadaddy.roadmap.step4_OOP.topic4_Car;
+
+public class Test {
+    public static void main(String[] args) {
+        Car car = new Car("BMW", "M5", 2024);
+        System.out.println(car.displayInfo());
+    }
+}
