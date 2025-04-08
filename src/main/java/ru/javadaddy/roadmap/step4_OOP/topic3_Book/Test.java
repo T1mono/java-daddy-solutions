57cb825e634fa88e4a007daa13ad7da61fbb1418
T1mon <godropopka@gmail.com>
2025-04-08 17:20:00 +0500

    Added package topic3_Book

---
+++ src/main/java/ru/javadaddy/roadmap/step4_OOP/topic3_Book/Test.java
@@ -0,0 +1,8 @@
+package ru.javadaddy.roadmap.step4_OOP.topic3_Book;
+
+public class Test {
+    public static void main(String[] args) {
+        Book book = new Book("War and Peace", "Tolstoy", 1867);
+        System.out.println(book.toString());
+    }
+}
