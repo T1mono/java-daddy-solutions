99d80a1c1c7e1df4d7de4141439a990ee17d12cf
T1mon <godropopka@gmail.com>
2025-04-10 12:57:25 +0500

    Added StringConcat

---
+++ src/main/java/ru/javadaddy/roadmap/step4_OOP/topic9_String/StringConcat/StringConcat.java
@@ -0,0 +1,10 @@
+package ru.javadaddy.roadmap.step4_OOP.topic9_String.StringConcat;
+
+public class StringConcat {
+    public static void main(String[] args) {
+        String firstString = "Hello ";
+        String secondString = "Gena!!!";
+
+        System.out.println(firstString + secondString);
+    }
+}
