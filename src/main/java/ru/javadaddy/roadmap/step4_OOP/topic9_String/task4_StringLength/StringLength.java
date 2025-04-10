4913d2c1be9ebb1a4f1955c55af774ebd409f322
T1mon <godropopka@gmail.com>
2025-04-10 14:32:12 +0500

    Added StringLength

---
+++ src/main/java/ru/javadaddy/roadmap/step4_OOP/topic9_String/task4_StringLength/StringLength.java
@@ -0,0 +1,8 @@
+package ru.javadaddy.roadmap.step4_OOP.topic9_String.task4_StringLength;
+
+public class StringLength {
+    public static void main(String[] args) {
+        String name = "Petya";
+        System.out.println(name.length());
+    }
+}
