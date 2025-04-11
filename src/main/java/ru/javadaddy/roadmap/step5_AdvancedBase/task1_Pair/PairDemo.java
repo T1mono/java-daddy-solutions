1c1e72f3ebef2fa0d47a4a383451739f43287230
T1mon <godropopka@gmail.com>
2025-04-11 12:47:36 +0500

    Added Pair and PairDemo

---
+++ src/main/java/ru/javadaddy/roadmap/step5_AdvancedBase/task1_Pair/PairDemo.java
@@ -0,0 +1,8 @@
+package ru.javadaddy.roadmap.step5_AdvancedBase.task1_Pair;
+
+public class PairDemo {
+    public static void main(String[] args) {
+        Pair<String, Integer> pair = new Pair<>("Петя", 1);
+        System.out.println(pair);
+    }
+}
