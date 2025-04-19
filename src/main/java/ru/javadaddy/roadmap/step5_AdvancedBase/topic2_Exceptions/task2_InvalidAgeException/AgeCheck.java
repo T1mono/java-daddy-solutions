b0b1e27c1c6eb2d7878ec837562aed02a85a0121
T1mono <167001770+T1mono@users.noreply.github.com>
2025-04-19 19:07:25 +0500

    Added AgeCheck and InvalidAgeException

--- src/main/java/ru/javadaddy/roadmap/step5_AdvancedBase/topic2_Exceptions/task2_InvalidAgeException/AgeCheck.java
+++ src/main/java/ru/javadaddy/roadmap/step5_AdvancedBase/topic2_Exceptions/task2_InvalidAgeException/AgeCheck.java
@@ -1,2 +1,27 @@
-package ru.javadaddy.roadmap.step5_AdvancedBase.topic2_Exceptions.task2_InvalidAgeException;public class AgeCheck {
+package ru.javadaddy.roadmap.step5_AdvancedBase.topic2_Exceptions.task2_InvalidAgeException;
+
+/**
+ * Напишите Java-программу, которая:
+ * Содержит пользовательское исключение InvalidAgeException, наследуемое от Exception.
+ * Имеет метод checkAge(int age), который выбрасывает InvalidAgeException, если возраст меньше 0 или больше 150.
+ * В методе main вызывайте метод checkAge() и обрабатывайте исключение с помощью try-catch, выводя сообщение об ошибке.
+ */
+public class AgeCheck {
+    public static void main(String[] args) {
+        try {
+            ageCheck(167);
+        } catch (InvalidAgeException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    public static void ageCheck(int age) throws InvalidAgeException {
+        if (age < 0) {
+            throw new InvalidAgeException("Возраст не может быть отрицательным: " + age);
+        } else if (age > 150) {
+            throw new InvalidAgeException("Возраст не может быть больше 150: " + age);
+        }
+        System.out.println("Возраст корректный: " + age);
+    }
 }
--- src/main/java/ru/javadaddy/roadmap/step5_AdvancedBase/topic2_Exceptions/task2_InvalidAgeException/InvalidAgeException.java
+++ src/main/java/ru/javadaddy/roadmap/step5_AdvancedBase/topic2_Exceptions/task2_InvalidAgeException/InvalidAgeException.java
@@ -1,2 +1,7 @@
-package ru.javadaddy.roadmap.step5_AdvancedBase.topic2_Exceptions.task2_InvalidAgeException;public class InvalidAgeException {
+package ru.javadaddy.roadmap.step5_AdvancedBase.topic2_Exceptions.task2_InvalidAgeException;
+
+public class InvalidAgeException extends Exception {
+    public InvalidAgeException(String message) {
+        super(message);
+    }
 }
