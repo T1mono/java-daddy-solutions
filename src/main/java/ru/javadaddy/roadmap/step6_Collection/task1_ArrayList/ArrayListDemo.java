8e901e65348d49b1b066d678ad38b9239002b046
T1mon <godropopka@gmail.com>
2025-04-21 08:54:27 +0500

    Added ArrayListDemo

---
+++ src/main/java/ru/javadaddy/roadmap/step6_Collection/task1_ArrayList/ArrayListDemo.java
@@ -0,0 +1,21 @@
+package ru.javadaddy.roadmap.step6_Collection.task1_ArrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        List<Integer> list  = new ArrayList<>();
+        list.add(12);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        list.remove(Integer.valueOf(12));
+        list.remove(1);
+
+        for (Integer number : list) {
+            System.out.println(list);
+        }
+    }
+}
