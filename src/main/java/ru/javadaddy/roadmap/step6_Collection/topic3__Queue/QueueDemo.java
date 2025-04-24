bab9cd2d55a01dc9d3f54d7a9692e6596b4ef088
T1mono <167001770+T1mono@users.noreply.github.com>
2025-04-24 21:19:38 +0500

    Added QueueDemo

---
+++ src/main/java/ru/javadaddy/roadmap/step6_Collection/topic3__Queue/QueueDemo.java
@@ -0,0 +1,20 @@
+package ru.javadaddy.roadmap.step6_Collection.topic3__Queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueDemo {
+    public static void main(String[] args) {
+        Queue<String> queue = new LinkedList<>();
+        queue.offer("A");
+        queue.offer("B");
+        queue.offer("C");
+
+        //Первый элемент без удаления
+        System.out.println(queue.peek());
+
+        while (!queue.isEmpty()) {
+            System.out.println(queue.poll());
+        }
+    }
+}
