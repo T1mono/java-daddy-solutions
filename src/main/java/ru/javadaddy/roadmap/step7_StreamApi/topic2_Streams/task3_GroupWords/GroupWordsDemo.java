988f1958398cd7007846c83af3c3c0e9b7f7478d
T1mon <godropopka@gmail.com>
2025-05-13 09:25:20 +0500

    Added GroupWordsDemo

---
+++ src/main/java/ru/javadaddy/roadmap/step7_StreamApi/topic2_Streams/task3_GroupWords/GroupWordsDemo.java
@@ -0,0 +1,24 @@
+package ru.javadaddy.roadmap.step7_StreamApi.topic2_Streams.task3_GroupWords;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * Напишите Java-программу, которая:
+ * Имеет список строк, например: ["apple", "ant", "banana", "berry", "cherry", "carrot"].
+ * Использует Stream API и Collectors.groupingBy для группировки строк по первой букве.
+ * Выводит полученную карту, где ключ – первая буква, а значение – список строк.
+ */
+public class GroupWordsDemo {
+    public static void main(String[] args) {
+        List<String> words = Arrays.asList("apple", "ant", "banana", "berry", "cherry", "carrot");
+
+        Map<Character, List<String>> groupWords = words.stream().
+                collect(Collectors.groupingBy(
+                        ch -> ch.charAt(0)
+                ));
+        System.out.println(groupWords);
+    }
+}
