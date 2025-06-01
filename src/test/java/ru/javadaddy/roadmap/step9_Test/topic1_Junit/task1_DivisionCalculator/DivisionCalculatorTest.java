ee8dfdec3e72eda5112d9189b7d0e1d1092c8fd6
T1mono <167001770+T1mono@users.noreply.github.com>
2025-06-01 17:43:35 +0500

    Потом объясню почему закомментил

--- src/test/java/ru/javadaddy/roadmap/step9_Test/topic1_Junit/task1_DivisionCalculator/DivisionCalculatorTest.java
+++ src/test/java/ru/javadaddy/roadmap/step9_Test/topic1_Junit/task1_DivisionCalculator/DivisionCalculatorTest.java
@@ -1,19 +1,20 @@
-package ru.javadaddy.roadmap.step9_Test.topic1_Junit.task1_DivisionCalculator;
-
-import org.junit.jupiter.api.Test;
-
-import static org.junit.Assert.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-class DivisionCalculatorTest {
-
-    @Test
-    void testDivision() {
-        assertEquals(5, DivisionCalculator.divide(20, 4));
-    }
-
-    @Test
-    void testDivisionByZero() {
-        assertThrows(ArithmeticException.class, () -> DivisionCalculator.divide(2, 0));
-    }
-}
+//package ru.javadaddy.roadmap.step9_Test.topic1_Junit.task1_DivisionCalculator;
+//
+//import org.junit.jupiter.api.Test;
+//import ru.javadaddy.roadmap.step9_Test.topic1_Junit.DivisionCalculator;
+//
+//import static org.junit.Assert.assertThrows;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//
+//class DivisionCalculatorTest {
+//
+//    @Test
+//    void testDivision() {
+//        assertEquals(5, DivisionCalculator.divide(20, 4));
+//    }
+//
+//    @Test
+//    void testDivisionByZero() {
+//        assertThrows(ArithmeticException.class, () -> DivisionCalculator.divide(2, 0));
+//    }
+//}
