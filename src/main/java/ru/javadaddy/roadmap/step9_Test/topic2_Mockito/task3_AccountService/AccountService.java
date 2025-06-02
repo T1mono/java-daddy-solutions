86547ea60eb5cfa243e48ffa49a085f5002178c8
T1mono <167001770+T1mono@users.noreply.github.com>
2025-06-02 20:30:31 +0500

    Added AccountServiceTest

--- src/main/java/ru/javadaddy/roadmap/step9_Test/topic2_Mockito/task3_AccountService/AccountService.java
+++ src/main/java/ru/javadaddy/roadmap/step9_Test/topic2_Mockito/task3_AccountService/AccountService.java
@@ -1,5 +1,28 @@
 package ru.javadaddy.roadmap.step9_Test.topic2_Mockito.task3_AccountService;
 
+/**
+ * Метод withdraw проверяет, достаточно ли средств на балансе, и уменьшает его.
+ * Используем Mockito для тестирования зависимостей AccountRepository.
+ *
+ * Написать тест на успешное снятие средств.
+ * Написать тест на отказ при недостатке средств.
+ * Написать тест на отказ, если аккаунт не найден.
+ * Использовать Mockito.verify() для проверки вызовов методов.
+ */
 public class AccountService {
+    private AccountRepository accountRepository;
 
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    public boolean withdraw(String accountId, double amount) {
+        Account account = accountRepository.findAccountById(accountId);
+        if (account != null && account.getBalance() >= amount) {
+            account.withdraw(amount);
+            accountRepository.updateAccount(account);
+            return true;
+        }
+        return false;
+    }
 }
---
+++ src/test/java/ru/javadaddy/roadmap/step9_Test/topic2_Mockito/task3_AccountService/AccountServiceTest.java
@@ -0,0 +1,60 @@
+package ru.javadaddy.roadmap.step9_Test.topic2_Mockito.task3_AccountService;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class AccountServiceTest {
+
+    @Mock
+    private AccountRepository accountRepository;
+
+    @InjectMocks
+    private AccountService accountService;
+
+    private Account account;
+
+    @BeforeEach
+    void setUp() {
+        account = new Account(
+                "111",
+                250_000.25
+        );
+    }
+
+    @Test
+    void withdrawSuccess() {
+        when(accountRepository.findAccountById(account.getAccountId())).thenReturn(account);
+        boolean result = accountService.withdraw(account.getAccountId(), account.getBalance());
+        assertTrue(result);
+        verify(accountRepository, times(1)).findAccountById(account.getAccountId());
+    }
+
+    @Test
+    void withdrawFailed() {
+        when(accountRepository.findAccountById(account.getAccountId())).thenReturn(account);
+        boolean result = accountService.withdraw(account.getAccountId(), 260_000.12);
+        assertFalse(result);
+        verify(accountRepository, times(1)).findAccountById(account.getAccountId());
+    }
+
+    @Test
+    void withdrawFailedAccountId() {
+        when(accountRepository.findAccountById("222")).thenReturn(account);
+        boolean result = accountService.withdraw("222", 260_000.12);
+        assertFalse(result);
+        verify(accountRepository, times(1)).findAccountById("222");
+    }
+
+
+}
